904b9794b50a660ce0cb4b05b5dcec42dc5bc0e6
wingyuen2022 <wingyuenuk330@gmail.com>
2022-06-06 15:09:45 +0100

    Updated basic stuff in PosController.js

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -7,14 +7,34 @@ router.get("/", (req, res) => {
     res.send("Get all post");
 });
 
-router.get("/:id", (req, res) => {
-    res.send("Get post by ID");
+router.post("/search", (req, res) => {
+    const data = req.body;
+    console.log(`Search a post: ${JSON.stringify(data)}`);
+    res.send("Search a post");
 });
 
 router.post("/", (req, res) => {
     const data = req.body;
     console.log(`Add post: ${JSON.stringify(data)}`);
     res.send("Create a post");
+});
+
+router.post("/reply", (req, res) => {
+    const data = req.body;
+    console.log(`Add reply to a post: ${JSON.stringify(data)}`);
+    res.send("Add reply to a post");
+});
+
+router.post("/emoji", (req, res) => {
+    const data = req.body;
+    console.log(`Add emoji to a post: ${JSON.stringify(data)}`);
+    res.send("Add emoji to a post");
+});
+
+router.post("/vote", (req, res) => {
+    const data = req.body;
+    console.log(`Add vote to a post: ${JSON.stringify(data)}`);
+    res.send("Add vote to a post");
 });
 
 module.exports = router;
