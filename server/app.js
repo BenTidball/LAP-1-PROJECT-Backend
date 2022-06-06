d63a56c6e1c730600998b4204907333f368b9500
Ben <bentidball97@gmail.com>
2022-06-06 13:51:32 +0100

    server fixes for test servers

--- server/app.js
+++ server/app.js
@@ -1,15 +1,11 @@
-const testGetRouter = require('../controllers/GetController');
 const express = require("express");
 const app = express();
-
-app.use('/test', testGetRouter);
+const testGetRouter = require('../controllers/GetController');
 
 app.get("/", (req, res) => {
-    console.log('test');
     res.send("backend test");
 });
 
-const port = 5500;
-app.listen(port, ()=> {
-    console.log(`Server is running on ${port}`);
-})
+app.use('/test', testGetRouter);
+
+module.exports = app;
