90c32739fe87525e3426b4e31d3d2d99d7dbc8d7
Ben <67053789+BenTidball@users.noreply.github.com>
2022-06-07 16:08:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./server/app');
 
-const port = process.env.PORT || 5000; // if there is no PORT env variable, 5000 will be used
+const port = process.env.PORT || 6000; // if there is no PORT env variable, 5000 will be used
 server.listen(port, () => console.log(`Express is running on port ${port}`))
