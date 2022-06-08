db9583a246c5e531ad5a129a4afc14b711e39041
Ben <67053789+BenTidball@users.noreply.github.com>
2022-06-08 10:56:36 +0100

    Update giphyAPI.js

--- controllers/giphyAPI.js
+++ controllers/giphyAPI.js
@@ -6,14 +6,12 @@ const apikey = 'KjhTKrHYVKk4fs2vueEKEy2poFC5QzPy';
 router.get("/", (req, res) => {
     fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apikey}&limit=5`)
     .then(response => response.json())
-    .then(data => console.log(data))
     .then(data => {res.send(data)});
 });
 
 router.get("/:id", (req, res) => {
     fetch(`https://api.giphy.com/v1/gifs?api_key=${apikey}&ids=21GmfgafQTNPL9oqD5`)
     .then(response => response.json())
-    .then(data => console.log(data))
     .then(data => {res.send(data)});
 });
 
