c22babb60a27a86f427317bb38293b1a483240d7
Ben <bentidball97@gmail.com>
2022-06-08 11:09:41 +0100

    stringify json data

--- controllers/giphyAPI.js
+++ controllers/giphyAPI.js
@@ -1,17 +1,20 @@
 const express = require('express')
+const fs = require('fs');
 const router = express.Router();
 
 const apikey = 'KjhTKrHYVKk4fs2vueEKEy2poFC5QzPy';
 
 router.get("/", (req, res) => {
     fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apikey}&limit=5`)
-    .then(response => response.json())
+    .then(response =>  response.json())
+    .then(data => JSON.stringify(data))
     .then(data => {res.send(data)});
 });
 
 router.get("/:id", (req, res) => {
     fetch(`https://api.giphy.com/v1/gifs?api_key=${apikey}&ids=21GmfgafQTNPL9oqD5`)
     .then(response => response.json())
+    .then(data => JSON.stringify(data))
     .then(data => {res.send(data)});
 });
 
